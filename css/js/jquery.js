42e40a401f571f3b4a3d8b698e1bf3e4686e4ad5
leandroGuedes110 <leandroguedes110@gmail.com>
2026-04-08 20:14:33 -0300

    Create jquery.js

---
+++ css/js/jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("#btn-detalhes").click(function() {
+    $("#detalhes").toggle();
+  });
+
+  $("#btn-limpar").click(function() {
+    $("#mensagem").text("");
+  });
+
+});
